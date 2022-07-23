0c6f137148feb4a1502cfb0dbaa45e4a02fd29a6
hollen9 <hollen.tw@gmail.com>
2022-07-24 05:03:22 +0800

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The Sidebar containing the main widget areas.
+ *
+ * @package vantage hollen9
+ */
+
+if ( ! in_array( siteorigin_page_setting( 'layout', 'default' ), array( 'default','full-width-sidebar' ), true ) ) return;
+?>
+<div id="secondary" class="widget-area" role="complementary">
+	<div>TEST SIDEBAR</div>
+	<?php do_action( 'before_sidebar' ); ?>
+	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	<?php do_action( 'after_sidebar' ); ?>
+</div><!-- #secondary .widget-area -->
