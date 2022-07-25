b2fd16604af44a764145d3698ef54a3b1d580307
hollen9 <hollen.tw@gmail.com>
2022-07-26 02:22:32 +0800

    Twitter Timeline Embed

--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,10 @@
 if ( ! in_array( siteorigin_page_setting( 'layout', 'default' ), array( 'default','full-width-sidebar' ), true ) ) return;
 ?>
 <div id="secondary" class="widget-area" role="complementary">
-	<div>TEST SIDEBAR</div>
 	<?php do_action( 'before_sidebar' ); ?>
+	<div>
+		<a class="twitter-timeline" data-tweet-limit="1" data-theme="dark" href="https://twitter.com/hollen9_tw?ref_src=twsrc%5Etfw">My Twitter</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+	</div><br/>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 	<?php do_action( 'after_sidebar' ); ?>
 </div><!-- #secondary .widget-area -->
