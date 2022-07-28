c7f023d516118c8b2207b11d6d491bc8a1d80be5
hollen9 <hollen.tw@gmail.com>
2022-07-28 16:30:52 +0800

    Move to helpers folder and rename to string-helper.php

--- php/includes/basic-functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Function for basic field validation (present and neither empty nor only white space
-function IsNullOrEmptyString($str){
-    return ($str === null || trim($str) === '');
-}
-
-
-?>
